936535630e05720706dbb1f6cae85c13cd498970
HeljaeRaeisaenen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2022-09-01 17:00:18 +0300

    ui

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 morgan.token('data', function (req, res) {
     return JSON.stringify(req.body)})
@@ -114,6 +117,6 @@ const generateId = () => {
     return maxId + 1}
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)})
